3f75fbbb486922f327ce2c450cedcfe1db043a12
rafih2001 <rafih2001@g.ucla.edu>
2023-04-14 11:34:16 -0700

    Added config_parser.cc, config_parser.h, and unit tests for config_parser; Modified CMakeLists.txt to include the config_parser library, link the server to this library, and include the config_parser_test executable
    Change-Id: Ib9f2356c501edb00433466ecab3ba304daf7d10c

---
+++ tests/config_parser_test.cc
@@ -0,0 +1,86 @@
+#include "gtest/gtest.h"
+#include "config_parser.h"
+
+class NginxConfigParserTest : public ::testing::Test {
+	protected:
+		void SetUp() override {
+			return;
+		}
+	NginxConfigParser parser;
+	NginxConfig out_config;
+};
+
+TEST_F(NginxConfigParserTest, SimpleConfig) {
+	bool success = parser.Parse("example_config",&out_config);
+	EXPECT_TRUE(success);
+}
+
+TEST_F(NginxConfigParserTest, CommentConfig) {
+	bool success = parser.Parse("example_config_2",&out_config);
+	EXPECT_TRUE(success);
+}
+
+TEST_F(NginxConfigParserTest, EmptyConfig) {
+	bool success = parser.Parse("example_config_3",&out_config);
+	EXPECT_TRUE(success);
+}
+
+TEST_F(NginxConfigParserTest, MultipleConfig) {
+	bool success = parser.Parse("example_config_4",&out_config);
+	EXPECT_TRUE(success);
+}
+
+TEST_F(NginxConfigParserTest, NestedConfig) {
+	bool success = parser.Parse("example_config_5",&out_config);
+	EXPECT_TRUE(success);
+}
+
+TEST_F(NginxConfigParserTest, BackSlashConfig) {
+	bool success = parser.Parse("example_config_6",&out_config);
+	EXPECT_TRUE(success);
+}
+
+TEST_F(NginxConfigParserTest, MissingBlockConfig) {
+	bool fail = parser.Parse("example_config_7",&out_config);
+	EXPECT_FALSE(fail);
+}
+
+TEST_F(NginxConfigParserTest, DoubleSemiColonConfig) {
+	bool fail = parser.Parse("example_config_8",&out_config);
+	EXPECT_FALSE(fail);
+}
+
+TEST_F(NginxConfigParserTest, MissingNameBlockConfig) {
+	bool fail = parser.Parse("example_config_9",&out_config);
+	EXPECT_FALSE(fail);
+}
+
+TEST_F(NginxConfigParserTest, MissingSemiColonConfig) {
+	bool fail = parser.Parse("example_config_10",&out_config);
+	EXPECT_FALSE(fail);
+}
+
+TEST_F(NginxConfigParserTest, MissingClosingBraceConfig) {
+	bool fail = parser.Parse("example_config_11",&out_config);
+	EXPECT_FALSE(fail);
+}
+
+TEST_F(NginxConfigParserTest, ExtraClosingBraceConfig) {
+	bool fail = parser.Parse("example_config_12",&out_config);
+	EXPECT_FALSE(fail);
+}
+
+TEST_F(NginxConfigParserTest, SingleQuoteBackslashEscape) {
+	bool success = parser.Parse("example_config_13",&out_config);
+	EXPECT_TRUE(success);
+}
+
+TEST_F(NginxConfigParserTest, DoubleQuoteBackslashEscape) {
+	bool success = parser.Parse("example_config_14",&out_config);
+	EXPECT_TRUE(success);
+}
+
+TEST_F(NginxConfigParserTest, NoWhiteSpaceAfterQuote) {
+	bool fail = parser.Parse("example_config_15",&out_config);
+	EXPECT_FALSE(fail);
+}
