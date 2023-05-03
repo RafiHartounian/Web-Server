d84b8b38965e2cd08c3bb02a0e189a0db9b8c9d5
Gregory Clark <gregclark@g.ucla.edu>
2023-05-03 04:54:03 +0000

    Add additional tests to session_test to get better coverage
    Change-Id: I9739a0df815e9dfe166f524f471b10d892ae4d55

--- tests/session_test.cc
+++ tests/session_test.cc
@@ -36,3 +36,37 @@ TEST_F(SessionFixture, SocketShutdown)
   boost::system::error_code err;
   EXPECT_TRUE(s.handle_write(err));
 }
+
+TEST_F(SessionFixture, GetStaticEndpoint) {
+  session s(io_service);
+  std::ostream os(&s.buf);
+  std::string example = "GET /static2/don.gif HTTP/1.1\r\nHost: www.wesbite.com\r\nConnection: close\r\n\r\n";
+  os << example;
+  std::string result = s.handle_read(boost::system::error_code(), example.size());
+  std::vector<path> configured_paths = {path{endpoint_type::static_, "/static2", "../static_files/static2"}};
+  s.set_configured_paths(configured_paths);
+  path expected;
+  expected.type = endpoint_type::static_;
+  expected.endpoint = "/static2";
+  expected.root = "../static_files/static2";
+  EXPECT_EQ(s.get_endpoint().type, expected.type);
+  EXPECT_EQ(s.get_endpoint().endpoint, expected.endpoint);
+  EXPECT_EQ(s.get_endpoint().root, expected.root);
+}
+
+TEST_F(SessionFixture, GetEchoEndpoint) {
+  session s(io_service);
+  std::ostream os(&s.buf);
+  std::string example = "GET /echo2 HTTP/1.1\r\nHost: www.wesbite.com\r\nConnection: close\r\n\r\n";
+  os << example;
+  std::string result = s.handle_read(boost::system::error_code(), example.size());
+  std::vector<path> configured_paths = {path{endpoint_type::echo, "/echo2", ""}};
+  s.set_configured_paths(configured_paths);
+  path expected;
+  expected.type = endpoint_type::echo;
+  expected.endpoint = "/echo2";
+  expected.root = "";
+  EXPECT_EQ(s.get_endpoint().type, expected.type);
+  EXPECT_EQ(s.get_endpoint().endpoint, expected.endpoint);
+  EXPECT_EQ(s.get_endpoint().root, expected.root);
+}
